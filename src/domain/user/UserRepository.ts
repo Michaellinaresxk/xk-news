a319be4a4e65ecf6f10965ea062d5f4c679c1179
Michael Linares <michaellinaresdbildungs@gmail.com>
2024-04-19 00:07:15 +0200

    Feature: Added User entity.

---
+++ src/domain/user/UserRepository.ts
@@ -0,0 +1,8 @@
+import type User from "./User";
+export default interface UserRepository {
+  registerUser(
+    email: string,
+    password: string,
+    userName: string
+  ): Promise<User>;
+}
---
+++ src/types/env.d.ts
@@ -0,0 +1,5 @@
+declare module "@env" {
+  export const NEWSAPI_KEY: string;
+  export const NYTAPI_KEY: string;
+  export const NEWSCREDAPI_KEY: string;
+}
